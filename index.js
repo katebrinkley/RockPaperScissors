301f0d9f57d6ff50c522ee63c778bf06d14c1dd6
monsterchild <jeremydejiacomo@gmail.com>
2017-10-24 21:33:58 -0400

    Starting Point for RPS

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 var userInput = document.getElementsByTagName ('button');
-var rockButton = document.getElementById ('rock');
-var paperButton = document.getElementById ('paper');
-var scissorButton = document.getElementById ('scissors');
 
 
-for(var i=0; i < userInput.length; i++) {
+	for(var i=0; i < userInput.length; i++) { 
+	 
+	  userInput[i].onclick = function () { 
 
-	userInput[i].onclick = function () {
-		var buttonClick = this.getAttribute('id');
-		}
-	}
+	  	var randomNumber = Math.floor(Math.random() * 3);
+	  	var options = ["rock", "paper", "scissors"];
+	    var buttonClick = this.getAttribute('id'); 
+	    var computerInput = options[randomNumber];
+
+
+	    console.log('userInput',buttonClick);
+	    console.log('computerInput',computerInput);
+
+	    if (buttonClick === "rock") {
+
+	    	if (computerInput === "paper") {
+	    		console.log('cpu wins');
+	    	} else if (computerInput === 'scissors') {
+	    		console.log('user wins');
+	    	} else if (computerInput === 'rock') {
+	    		console.log('its a tie');
+	    	}
+	    	
+	    } 
+	} 
 }
-
-
+ 
+ 
 //Math.floor(Math.random() * maxNumber)
